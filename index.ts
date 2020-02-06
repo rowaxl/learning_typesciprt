9c95ae6889019e2ebeb0be211057682128eee167
rowaxl <rowaxl0@gmail.com>
2020-02-07 00:06:43 +0700

    add interface and implements class pattern

--- index.ts
+++ index.ts
@@ -1,14 +1,35 @@
 import axios from "axios"
 
-type Todo = {
+interface ITodo {
   userId: number
   id: number
   title: string
   completed: boolean
 }
 
-axios.get("https://jsonplaceholder.typicode.com/todos/1").then(res => {
-  const todo: Todo = res.data
+class TODO implements ITodo {
+  constructor(todo: ITodo) {
+    this.userId = todo.userId
+    this.id = todo.id
+    this.title = todo.title
+    this.completed = todo.completed
+  }
 
-  console.log(todo)
+  public userId: number
+  public id: number
+  public title: string
+  public completed: boolean
+  public printTodo = () => {
+    console.log(`
+      id: ${this.id}
+      title: ${this.title}
+      completed: ${this.completed}
+    `)
+  }
+}
+
+axios.get("https://jsonplaceholder.typicode.com/todos/1").then(res => {
+  const todo = new TODO(res.data)
+
+  todo.printTodo()
 })
