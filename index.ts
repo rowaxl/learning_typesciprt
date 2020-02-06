a4600bd538b8bea779e9d526e8bfd98c1a216a11
rowaxl <rowaxl0@gmail.com>
2020-02-06 23:56:20 +0700

    use type

--- index.ts
+++ index.ts
@@ -1,5 +1,14 @@
-import axios from 'axios'
+import axios from "axios"
 
-axios.get('https://jsonplaceholder.typicode.com/todos/1').then(res => {
-  console.log(res.data)
+type Todo = {
+  userId: number
+  id: number
+  title: string
+  completed: boolean
+}
+
+axios.get("https://jsonplaceholder.typicode.com/todos/1").then(res => {
+  const todo: Todo = res.data
+
+  console.log(todo)
 })
