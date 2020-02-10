cfef07929802fe8aede5c666bf6e2f5af4e48462
rowaxl <rowaxl0@gmail.com>
2020-02-10 17:35:44 +0700

    add render marker methods

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,5 +1,6 @@
 import { User } from './User'
 import { Company } from './Company'
+import { CustomMap } from './CustomMap'
 
 const newUser = new User()
 console.log('user: ', newUser)
@@ -7,11 +8,6 @@ console.log('user: ', newUser)
 const newCompany = new Company()
 console.log('company: ', newCompany)
 
-// render google map
-new google.maps.Map(document.getElementById('map'), {
-  zoom: 5,
-  center: {
-    lat: newUser.location.lat,
-    lng: newUser.location.lng
-  }
-})
+const googleMap = new CustomMap('map')
+googleMap.addMarker(newUser)
+googleMap.addMarker(newCompany)
