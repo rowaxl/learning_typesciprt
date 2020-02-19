5880f9033094baec0b7c4125f5501aa4c01384b8
rowaxl <rowaxl0@gmail.com>
2020-02-20 00:13:58 +0900

    linked list sort

--- sorting/src/index.ts
+++ sorting/src/index.ts
@@ -1,15 +1,17 @@
 import { Sorter } from './Sorter'
 import { NumberCollection } from './NumberCollection'
 import { CharactersCollection } from './CharactersCollection'
+import { LinkedList } from './LinkedList'
 
-const initNums = [0,-1,2,1]
+const initNums = [0, -1, 2, 1]
+console.log(`
+before: ${initNums}`)
+
 const numCol = new NumberCollection(initNums)
 const sortNum = new Sorter(numCol)
 
 sortNum.simpleSort()
-console.log(`
-before: ${initNums}
-after: ${numCol.data}
+console.log(`after: ${numCol.data}
 `)
 
 const initChar = 'bbasXe2mlvsa'
@@ -17,7 +19,22 @@ const charCol = new CharactersCollection(initChar)
 const sortStr = new Sorter(charCol)
 
 sortStr.simpleSort()
-console.log(`
-before: ${initChar}
+console.log(`before: ${initChar}
 after: ${charCol.data}
 `)
+
+const linkedList = new LinkedList()
+linkedList.add(5)
+linkedList.add(999)
+linkedList.add(2)
+linkedList.add(10)
+linkedList.add(-10)
+
+console.log(`before sort`)
+linkedList.print()
+
+const sortLinkedList = new Sorter(linkedList)
+sortLinkedList.simpleSort()
+console.log(`
+after sort`)
+linkedList.print()
