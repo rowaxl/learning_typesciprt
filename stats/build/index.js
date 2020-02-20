7c36f726036b5eb855b9201f5aa1f5eb0672bf3f
rowaxl <rowaxl0@gmail.com>
2020-02-20 09:16:55 +0900

    set prototype

--- stats/build/index.js
+++ stats/build/index.js
@@ -9,9 +9,23 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = __importStar(require("fs"));
 var assetPath = './asset/football.csv';
-var data = fs.readFileSync(assetPath, {
+var matches = fs.readFileSync(assetPath, {
     encoding: 'UTF-8'
 })
     .split('\n')
     .map(function (row) { return row.split(','); });
-console.log(data);
+/**
+ * e.g. [ '28/10/2018', 'Man United', 'Everton', '2', '1', 'H', 'J Moss' ]
+ * date, home team, away team, , , which won,
+ */
+var manUnitedWins = 0;
+for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
+    var match = matches_1[_i];
+    if (match[1] === 'Man United' && match[5] === 'H') {
+        manUnitedWins++;
+    }
+    if (match[2] === 'Man United' && match[5] === 'A') {
+        manUnitedWins++;
+    }
+}
+console.log("Man United wins " + manUnitedWins + " times");
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,10 +1,28 @@
 import * as fs from 'fs'
 
 const assetPath = './asset/football.csv'
-const data = fs.readFileSync(assetPath, {
+const matches = fs.readFileSync(assetPath, {
   encoding: 'UTF-8'
 })
   .split('\n')
-  .map((row: string) => row.split(','))
+  .map((row: string):string[] => row.split(','))
 
-console.log(data)
+
+/**
+ * e.g. [ '28/10/2018', 'Man United', 'Everton', '2', '1', 'H', 'J Moss' ]
+ * date, home team, away team, , , which won, 
+ */
+
+let manUnitedWins = 0
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins++
+  }
+
+  if (match[2] === 'Man United' && match[5] === 'A') {
+    manUnitedWins++
+  }
+}
+
+console.log(`Man United wins ${manUnitedWins} times`)
