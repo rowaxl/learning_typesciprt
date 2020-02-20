1e6cb96cab993b0288011cab6abecda9e4fca03c
rowaxl <rowaxl0@gmail.com>
2020-02-20 09:10:00 +0900

    add read csv files

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log('hello world');
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = __importStar(require("fs"));
+var assetPath = './asset/football.csv';
+var data = fs.readFileSync(assetPath, {
+    encoding: 'UTF-8'
+});
+console.log(data);
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,8 @@
-console.log('hello world')
+import * as fs from 'fs'
+
+const assetPath = './asset/football.csv'
+const data = fs.readFileSync(assetPath, {
+  encoding: 'UTF-8'
+})
+
+console.log(data)
