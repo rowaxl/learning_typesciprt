260faad82549732a3deb974f3b82cced04e481b6
rowaxl <rowaxl0@gmail.com>
2020-04-10 14:12:59 -0700

    refactor by interface pattern

--- stats/src/CSVReader.ts
+++ stats/src/CSVReader.ts
@@ -1,11 +1,24 @@
 import * as fs from 'fs'
+import { MatchData } from './MatchData'
+import { MatchResult } from './MatchResult'
+import { strToDate } from './utils'
 
-export abstract class CSVReader<T> {
-  public data: T[] = []
+export class CSVReader {
+  public data: MatchData[] = []
 
   constructor(public filename: string) {}
 
-  abstract mapRow(row: string[]): T
+  mapRow(row: string[]): MatchData {
+    return [
+      strToDate(row[0]).toDateString(),
+      row[1],
+      row[2],
+      parseInt(row[3]),
+      parseInt(row[4]),
+      row[5] as MatchResult,
+      row[6]
+    ]
+  }
 
   read() {
     this.data = fs
--- stats/src/MatchReader.ts
+++ stats/src/MatchReader.ts
@@ -1,18 +1,8 @@
-import { CSVReader } from './CSVReader'
-import { MatchData } from './MatchData'
-import { MatchResult } from './MatchResult'
-import { strToDate } from './utils'
+interface DataReader {
+  read(): void
+  data: string[][]
+}
 
-export class MatchReader extends CSVReader<MatchData> {
-  mapRow(row: string[]): MatchData {
-    return [
-      strToDate(row[0]).toDateString(),
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      row[5] as MatchResult,
-      row[6]
-    ]
-  }
+export class MatchReader {
+  constructor(public reader: DataReader) {}
 }
